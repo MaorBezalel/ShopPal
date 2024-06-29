0b12da584adc1c91f1e76496db806628762e4610
Maor <maorbz858@gmail.com>
2024-06-30 01:02:23 +0300

    Added JSDocs

--- server/src/shared/utils/helpers.ts
+++ server/src/shared/utils/helpers.ts
@@ -1,6 +1,16 @@
 import { Class } from '@/shared/types/utils.types';
 
+/**
+ * Helper class for transforming data from and to Postgres composite types
+ */
 export class PGDataTransformer {
+    /**
+     * Transforms data from Postgres composite type to a class instance (Embedded Entity)
+     *
+     * @template TEmbeddedEntity - Embedded Entity class type (e.g. Address, NameDetails, etc.)
+     * @param toClass - class to transform data to
+     * @returns - function that transforms data from Postgres composite type to a class instance
+     */
     static fromPGCompositeType<TEmbeddedEntity extends object>(toClass: Class<TEmbeddedEntity>): (value: any) => any {
         const instance = new toClass();
         const fields = Object.keys(instance);
@@ -21,6 +31,13 @@ export class PGDataTransformer {
         };
     }
 
+    /**
+     * Transforms data from class instance (Embedded Entity) to Postgres composite type
+     *
+     * @template TEmbeddedEntity - Embedded Entity class type (e.g. Address, NameDetails, etc.)
+     * @param fromClass - class to transform data from
+     * @returns - function that transforms data from class instance to Postgres composite type
+     */
     static toPGCompositeType<TEmbeddedEntity extends object>(fromClass: Class<TEmbeddedEntity>): (value: any) => any {
         return (data: new () => any) => {
             const values = Object.values(data).filter((value) => value !== undefined);
