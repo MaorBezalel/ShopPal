10a4afc1bb696f9962af6aa6058c37f94d1c8490
Maor <maorbz858@gmail.com>
2024-07-26 18:37:51 +0300

    Refactor `Accordion` component to accept custom button and styles

--- client/src/shared/components/Accordion.tsx
+++ client/src/shared/components/Accordion.tsx
@@ -1,35 +1,60 @@
-import React from 'react';
-
-import { useState } from 'react';
-
-import minusSvg from '@/assets/svgs/minus.svg';
-import plusSvg from '@/assets/svgs/plus.svg';
+import React, { useState, ReactElement, cloneElement } from 'react';
 
 type AccordionProps = {
-    title: string;
     children: React.ReactNode;
-}
+    accordionButton: ReactElement | ((isOpen: boolean) => ReactElement);
+    accordionStyles?: string;
+    isOpen?: boolean;
+    onToggle?: (isOpen: boolean) => void;
+};
 
-const Accordion: React.FC<AccordionProps> = ({ title, children }: AccordionProps) => {
+export default function Accordion({
+    children,
+    isOpen: controlledIsOpen,
+    onToggle,
+    accordionStyles = '',
+    accordionButton,
+}: AccordionProps) {
     const [isOpen, setIsOpen] = useState<boolean>(false);
+    const isControlled = controlledIsOpen !== undefined;
+    const currentIsOpen = isControlled ? controlledIsOpen : isOpen;
+
+    const handleToggle = () => {
+        if (isControlled) {
+            onToggle && onToggle(!controlledIsOpen);
+        } else {
+            setIsOpen((prev) => {
+                onToggle?.(!prev);
+                return !prev;
+            });
+        }
+    };
+
+    const buttonToRender = () => {
+        const additionalProps = {
+            onClick: handleToggle,
+            'aria-expanded': currentIsOpen,
+        };
+
+        return typeof accordionButton === 'function'
+            ? cloneElement(accordionButton(currentIsOpen), additionalProps)
+            : cloneElement(accordionButton, additionalProps);
+    };
 
     return (
-        <div className='py-2'>
-            <button onClick={() => setIsOpen((prev) => !prev)} className='flex justify-between w-full items-center'>
-                <span>{title}</span>
-                <span><img className='w-3 h-3' src={isOpen ? minusSvg : plusSvg}/></span>
-            </button>
+        <div className={accordionStyles}>
+            {/* Accordion Button */}
+            {buttonToRender()}
 
-            <div className={`grid overflow-hidden transition-all duration-300 ease-in-out ${
-                isOpen 
-                ? 'grid-rows-[1fr] opacity-100'
-                : 'grid-rows-[0fr] opacity-0' 
-            }`}>
-
-                <div className='overflow-hidden'>{children}</div>
+            {/*  Accordion Content */}
+            <div
+                className={`grid overflow-hidden transition-all duration-300 ease-in-out ${
+                    currentIsOpen ? 'grid-rows-[1fr] opacity-100' : 'grid-rows-[0fr] opacity-0'
+                }`}
+                aria-hidden={!currentIsOpen}
+            >
+                <div className="overflow-hidden">{children}</div>
             </div>
         </div>
     );
-};
-
-export default Accordion;
+}
